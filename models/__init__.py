327861e4722b0c961caaf4486fe607622ead704b
Rashid <abdirashidabubakar7@gmail.com>
2024-07-25 02:08:03 -0700

    let me see

--- models/__init__.py
+++ models/__init__.py
@@ -8,10 +8,9 @@ models package initialization file.
 This file allows the models package to be imported and initializes the package.
 """
 
-# from .base_model import BaseModel
+
 from models.engine.file_storage import FileStorage
 
 
-# __all__ = ["BaseModel"]
 storage = FileStorage()
 storage.reload()
