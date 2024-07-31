aea852389b07bcf63feedf111c82831fc354baec
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-31 17:04:43 +0300

    updating file storage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,7 +48,11 @@ class FileStorage:
                 for obj_dict in json_objects.values():
                     class_name = obj_dict["__class__"]
                     del obj_dict["__class__"]
-                    module = __import__('models.base_model',
-                                        fromlist=[class_name])
-                    cls = getattr(module, class_name)
+                    # module = __import__('models.base_model',
+                    #                     fromlist=[class_name])
+                    if class_name == "BaseModel":
+                        cls = BaseModel
+                    elif class_name == "User":
+                        cls = User
+                    # cls = getattr(module, class_name)
                     self.new(cls(**obj_dict))
